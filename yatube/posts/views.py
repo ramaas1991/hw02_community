3ccc4a80e0974a89233630cab2fdc62058299c1e
Shishkin Alexander <ramaas91@gmail.com>
2022-08-12 22:37:50 +0300

    yatubeproject

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -22,7 +22,6 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all().order_by('-pub_date')[:Pub_limit]
-    title = 'заголовок'
     context = {
         'group': group,
         'posts': posts,
